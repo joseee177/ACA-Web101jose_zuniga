1197a1d1122c7f0ab83158280dd6ca909648a1f9
joseee177 <josezuniga1517@gmail.com>
2020-09-02 19:44:44 -0500

    merging to turn in assignment

---
+++ resume/resume.js
@@ -0,0 +1,12 @@
+function myanimation (){
+  document.getElementById("demo").innerHTML = "512-459-2222";
+}
+document.getElementById("atx").onclick = function(){
+  myfunction()
+};
+function myfunction(){
+  document.getElementById("atx").innerHTML=null;
+}
+function userclick(element){
+  
+}
